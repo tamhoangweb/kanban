4ddcb7eab54f38566d888cae6f27d3c0b3924e62
tamhoangweb <tamhoangweb@gmail.com>
2022-07-27 10:32:59 +0700

    Finish Login and Signup

---
+++ client/src/components/layout/AppLayout.jsx
@@ -0,0 +1,45 @@
+import { Box } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import authUtils from "../../utils/authUtils";
+import Loading from "../common/Loading";
+
+const AppLayout = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const user = await authUtils.isAuthenticated();
+      if (!user) {
+        navigate("/login");
+      } else {
+        // save user
+        setLoading(false);
+      }
+    };
+    checkAuth();
+  }, [navigate]);
+
+  return loading ? (
+    <Loading fullHeight />
+  ) : (
+    <Box
+      sx={{
+        display: "flex",
+      }}
+    >
+      <Box
+        sx={{
+          flexGrow: 1,
+          p: 1,
+          width: "max-content",
+        }}
+      >
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+
+export default AppLayout;
